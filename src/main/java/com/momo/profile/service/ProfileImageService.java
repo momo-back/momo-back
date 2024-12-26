147307cac3e810baf28abdaffa4ed5c0605033bb
seokjin <wlsdldos00@gmail.com>
2024-12-26 09:23:17 +0900

    feat: 프로필 이미지 관련 기능을 담당하는 ProfileImageService 구현
    - 이미지가 존재하면 uploadImage 메서드를 실행하고 없으면 DEFAULT_IMAGE_KEY를 반환하는 getProfileImageUrl 메서드 구현
    - 이미지 검증과 업로드를 담당하는 uploadImage 메서드 구현
    - 이미지 검증을 담당하는 validateImageFormat 메서드 구현

---
+++ src/main/java/com/momo/profile/service/ProfileImageService.java
@@ -0,0 +1,38 @@
+package com.momo.profile.service;
+
+import com.momo.profile.adptor.ImageStorage;
+import com.momo.profile.constant.ImageType;
+import com.momo.profile.exception.ProfileErrorCode;
+import com.momo.profile.exception.ProfileException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileImageService {
+
+  private static final String DEFAULT_IMAGE_KEY =
+      "https://s3-momo-storage.s3.ap-northeast-2.amazonaws.com/default.jpg";
+
+  private final ImageStorage imageStorage;
+
+  public String getProfileImageUrl(MultipartFile profileImage) {
+    return Optional.ofNullable(profileImage)
+        .filter(imageFile -> !imageFile.isEmpty())
+        .map(this::uploadImage)
+        .orElse(DEFAULT_IMAGE_KEY);
+  }
+
+  private String uploadImage(MultipartFile profileImage) {
+    validateImageFormat(profileImage.getContentType(), profileImage.getOriginalFilename());
+    return imageStorage.uploadImage(profileImage);
+  }
+
+  private void validateImageFormat(String contentType, String filename) {
+    if (!ImageType.isSupported(contentType, filename)) {
+      throw new ProfileException(ProfileErrorCode.INVALID_IMAGE_FORMAT);
+    }
+  }
+}
