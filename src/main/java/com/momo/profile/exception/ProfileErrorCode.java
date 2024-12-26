9740c69aafafb5e1b33c420e226b8d7e72c568a6
seokjin <wlsdldos00@gmail.com>
2024-12-26 09:27:55 +0900

    feat: 프로필 관련 에러 코드 정의

---
+++ src/main/java/com/momo/profile/exception/ProfileErrorCode.java
@@ -0,0 +1,18 @@
+package com.momo.profile.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProfileErrorCode {
+
+  DUPLICATE_PROFILE("이미 프로필이 존재합니다.", HttpStatus.CONFLICT),
+  INVALID_GENDER("성별을 입력해 주세요.", HttpStatus.BAD_REQUEST),
+  INVALID_BIRTH("생년월일을 입력해 주세요.", HttpStatus.BAD_REQUEST),
+  INVALID_IMAGE_FORMAT("지원하지 않는 이미지 형식입니다.", HttpStatus.BAD_REQUEST);
+
+  private final String message;
+  private final HttpStatus status;
+}
