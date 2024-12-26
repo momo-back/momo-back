0600ecf202dbc8679ef9b18436368824377f0c39
seokjin <wlsdldos00@gmail.com>
2024-12-26 10:10:36 +0900

    feat: 성별과 mbti를 정의하는 enum 생성

---
+++ src/main/java/com/momo/profile/constant/Mbti.java
@@ -0,0 +1,12 @@
+package com.momo.profile.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum Mbti {
+  INFP, INFJ, INTP, INTJ,
+  ISFP, ISFJ, ISTP, ISTJ,
+  ENFP, ENFJ, ENTP, ENTJ,
+  ESFP, ESFJ, ESTP, ESTJ,
+  NONE;
+}
