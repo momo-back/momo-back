9e800f6955c2cdd0c2e72c4ec2c8f5c50773dbfc
seokjin <wlsdldos00@gmail.com>
2024-12-26 10:13:16 +0900

    feat: 날짜 자동 생성을 위한 BaseEntity 구현

---
+++ src/main/java/com/momo/profile/persist/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.momo.profile.persist.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
+}
