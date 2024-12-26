0ceb8648b19376dcb5c776ca26e160e5d203605a
seokjin <wlsdldos00@gmail.com>
2024-12-26 09:21:24 +0900

    feat: ProfileService 구현
    - 프로필 생성 로직을 담은 createProfile 메서드 구현
    - 추후에 회원 검증 필요
    - ProfileResponse에 이미지 url을 포함하여 반환

---
+++ src/main/java/com/momo/profile/service/ProfileService.java
@@ -0,0 +1,38 @@
+package com.momo.profile.service;
+
+import com.momo.profile.dto.ProfileCreateRequest;
+import com.momo.profile.dto.ProfileCreateResponse;
+import com.momo.profile.persist.entity.Profile;
+import com.momo.profile.persist.repository.ProfileRepository;
+import com.momo.profile.validate.ProfileRequiredValueValidator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+  private final ProfileRepository profileRepository;
+  private final ProfileImageService profileImageService;
+
+  // TODO: 회원 ID 매개변수로 받아야 함
+  public ProfileCreateResponse createProfile(
+      ProfileCreateRequest request, MultipartFile profileImage
+  ) {
+    // TODO: 회원 존재 여부 검증
+    // TODO: 이미 프로필이 존재하는지 여부 검증
+
+    ProfileRequiredValueValidator.profileRequiredValueValidate(
+        request.getGender(), request.getBirth()
+    );
+
+    String profileImageUrl = profileImageService.getProfileImageUrl(profileImage);
+    Profile profile = request.toEntity(profileImageUrl);
+
+    Profile savedProfile = profileRepository.save(profile);
+    return ProfileCreateResponse.from(savedProfile);
+  }
+}
