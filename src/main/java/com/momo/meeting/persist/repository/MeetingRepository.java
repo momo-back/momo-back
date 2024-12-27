4adf6d0f460bb0994a72e39e6fa3018e10b48776
seokjin <wlsdldos00@gmail.com>
2024-12-28 01:57:04 +0900

    feat: MeetingRepository 구현

---
+++ src/main/java/com/momo/meeting/persist/repository/MeetingRepository.java
@@ -0,0 +1,12 @@
+package com.momo.meeting.persist.repository;
+
+import com.momo.meeting.persist.entity.Meeting;
+import java.time.LocalDateTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+
+  long countByUser_IdAndCreatedAtBetween(Long userId, LocalDateTime startOfDay, LocalDateTime endOfDay);
+}
