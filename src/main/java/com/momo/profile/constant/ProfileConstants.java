e4fc280d76a52f166282ade00a2f65a0d5838f56
seokjin <wlsdldos00@gmail.com>
2024-12-28 22:17:14 +0900

    refactor: 프로필 관련되어 사용될 상수값 분리

---
+++ src/main/java/com/momo/profile/constant/ProfileConstants.java
@@ -0,0 +1,10 @@
+package com.momo.profile.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ProfileConstants {
+
+  public static final String DEFAULT_IMAGE_URL =
+      "https://s3-momo-storage.s3.ap-northeast-2.amazonaws.com/default.jpg";
+}
--- src/main/java/com/momo/profile/service/ProfileImageService.java
+++ src/main/java/com/momo/profile/service/ProfileImageService.java
@@ -2,6 +2,7 @@ package com.momo.profile.service;
 
 import com.momo.profile.adptor.ImageStorage;
 import com.momo.profile.constant.ImageType;
+import com.momo.profile.constant.ProfileConstants;
 import com.momo.profile.exception.ProfileErrorCode;
 import com.momo.profile.exception.ProfileException;
 import java.util.Optional;
@@ -13,16 +14,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class ProfileImageService {
 
-  private static final String DEFAULT_IMAGE_URL =
-      "https://s3-momo-storage.s3.ap-northeast-2.amazonaws.com/default.jpg";
-
   private final ImageStorage imageStorage;
 
-  public String getProfileImageUrl(MultipartFile profileImage) {
+  public String createProfileImageUrl(MultipartFile profileImage) {
     return Optional.ofNullable(profileImage)
         .filter(imageFile -> !imageFile.isEmpty())
         .map(this::uploadImage)
-        .orElse(DEFAULT_IMAGE_URL);
+        .orElse(ProfileConstants.DEFAULT_IMAGE_URL);
   }
 
   private String uploadImage(MultipartFile profileImage) {
