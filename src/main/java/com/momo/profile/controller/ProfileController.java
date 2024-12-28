6cc5b9c888cb2b98677e0b7d3cdc5dee694a04b1
seokjin <wlsdldos00@gmail.com>
2024-12-28 22:18:42 +0900

    feat: uri 작성 관례에 맞게 복수형으로 수정 및 created를 응답으로 반환

--- src/main/java/com/momo/profile/controller/ProfileController.java
+++ src/main/java/com/momo/profile/controller/ProfileController.java
@@ -1,9 +1,13 @@
 package com.momo.profile.controller;
 
 import com.momo.profile.dto.ProfileCreateRequest;
+import com.momo.profile.dto.ProfileCreateResponse;
 import com.momo.profile.service.ProfileService;
+import com.momo.user.dto.CustomUserDetails;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -11,18 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1/profile")
+@RequestMapping("/api/v1/profiles")
 @RequiredArgsConstructor
 public class ProfileController {
 
   private final ProfileService profileService;
 
-  // TODO: 로그인된 사용자만 가능하도록 Authorization 필요
   @PostMapping
-  public ResponseEntity<?> createProfile(
+  public ResponseEntity<ProfileCreateResponse> createProfile(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails,
       @RequestPart ProfileCreateRequest request,
       @RequestPart(required = false) MultipartFile profileImage
   ) {
-    return ResponseEntity.ok(profileService.createProfile(request, profileImage));
+    ProfileCreateResponse response = profileService.createProfile(
+        customUserDetails.getId(),
+        request,
+        profileImage
+    );
+    return ResponseEntity
+        .created(URI.create("/api/profiles/" + response.getId()))
+        .body(response);
   }
 }
