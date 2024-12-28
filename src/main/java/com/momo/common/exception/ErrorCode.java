548cbd01a6bea9a710f1a2adee6cc61c31f5adcc
seokjin <wlsdldos00@gmail.com>
2024-12-29 02:37:06 +0900

    feat: 프로필 생성하지 않았을 때 발생하는 에러코드 추가

--- src/main/java/com/momo/common/exception/ErrorCode.java
+++ src/main/java/com/momo/common/exception/ErrorCode.java
@@ -4,7 +4,8 @@ public enum ErrorCode {
   INVALID_VERIFICATION_TOKEN("유효하지 않은 인증 토큰입니다.", 400),
   VALIDATION_ERROR("잘못된 요청입니다", 400),
   DUPLICATE_ERROR("이미 사용 중인 값입니다", 409),
-  INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다", 500);
+  INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다", 500),
+  NOT_EXISTS_PROFILE("프로필 생성을 완료해 주세요.", 403);
 
   private final String message;
   private final int status;
