ee7ab32742f562011b57f4d05575bc71cde05ece
seokjin <wlsdldos00@gmail.com>
2024-12-30 23:29:19 +0900

    fix:누락된 import  추가

--- src/main/java/com/momo/meeting/controller/MeetingController.java
+++ src/main/java/com/momo/meeting/controller/MeetingController.java
@@ -1,5 +1,6 @@
 package com.momo.meeting.controller;
 
+import com.momo.common.annotation.RequireProfile;
 import com.momo.meeting.dto.MeetingCreateRequest;
 import com.momo.meeting.dto.MeetingCreateResponse;
 import com.momo.meeting.service.MeetingService;
