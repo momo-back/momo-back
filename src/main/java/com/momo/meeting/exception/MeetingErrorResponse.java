d5d5d348c1bb5b028958b61ddd1e6a29eaafeb16
seokjin <wlsdldos00@gmail.com>
2024-12-31 04:17:20 +0900

    feat: MeetingExceptionHandler 클래스에서 @Valid 어노테이션에서 발생하는 에러를 검증하고 모든 에러 필드 정보를 반환하도록 수정

--- src/main/java/com/momo/meeting/exception/MeetingErrorResponse.java
+++ src/main/java/com/momo/meeting/exception/MeetingErrorResponse.java
@@ -1,6 +1,7 @@
 package com.momo.meeting.exception;
 
-import java.util.Map;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,5 +9,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MeetingErrorResponse {
 
-  private final Map<String, String> errors;
+  private final int status;
+  private final String message;
+  private final List<FieldError> errors;
+
+  @Getter
+  @AllArgsConstructor
+  public static class FieldError {
+
+    private String field;
+    private String message;
+    private Object rejectedValue;
+  }
 }
--- src/main/java/com/momo/meeting/validator/MeetingValidator.java
+++ src/main/java/com/momo/meeting/validator/MeetingValidator.java
@@ -1,5 +1,7 @@
 package com.momo.meeting.validator;
 
+import com.momo.common.exception.CustomException;
+import com.momo.common.exception.ErrorCode;
 import com.momo.meeting.exception.MeetingErrorCode;
 import com.momo.meeting.exception.MeetingException;
 import com.momo.meeting.repository.MeetingRepository;
@@ -15,16 +17,13 @@ public class MeetingValidator {
 
   private final MeetingRepository meetingRepository;
   private final UserRepository userRepository;
-  //private final UserValidator userValidator;
 
   public void validateForMeetingCreation(Long userId, LocalDateTime meetingDateTime) {
     // TODO: 일일 모임 생성 제한 검증 Redis 고려
     validateDailyPostLimit(userId);
     validateMeetingDateTime(meetingDateTime);
-    // TODO: 프로필 생성됐는지 검증 필요
-    //userValidator.existsById(userId); // TODO: merge 후 변경
     if (!userRepository.existsById(userId)) {
-      throw new RuntimeException();
+      throw new CustomException(ErrorCode.USER_NOT_FOUND);
     }
   }
 
@@ -32,9 +31,9 @@ public class MeetingValidator {
     LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
     LocalDateTime endOfDay = startOfDay.plusDays(1);
 
-    int todayPostCount = meetingRepository.countByUser_IdAndCreatedAtBetween(userId, startOfDay,
-        endOfDay);
-
+    int todayPostCount = meetingRepository.countByUser_IdAndCreatedAtBetween(
+        userId, startOfDay, endOfDay
+    );
     if (todayPostCount >= 10) {
       throw new MeetingException(MeetingErrorCode.DAILY_POSTING_LIMIT_EXCEEDED);
     }
