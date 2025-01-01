b9a0030232bbf7ad77c39842feff1b0285cb9089
seokjin <wlsdldos00@gmail.com>
2025-01-02 03:11:54 +0900

    feat: MeetingException을 처리하는 핸들러 메서드 추가

--- src/main/java/com/momo/meeting/exception/MeetingExceptionHandler.java
+++ src/main/java/com/momo/meeting/exception/MeetingExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.momo.meeting.exception;
 
+import com.momo.common.exception.CustomException;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,24 +13,37 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE) // 높은 우선순위로 설정
+@RestControllerAdvice(basePackages = "com.momo.meeting")
 public class MeetingExceptionHandler {
 
+  @ExceptionHandler(MeetingException.class)
+  public ResponseEntity<MeetingErrorResponse> handleMeetingException(MeetingException e) {
+    return createErrorResponse(e.getMeetingErrorCode());
+  }
+
+  /**
+   * DTO 클래스에서 검증 어노테이션으로 검증 시도 시, 유효하지 않으면 MethodArgumentNotValidException 발생
+   *
+   * @param e
+   * @return
+   */
+
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<MeetingErrorResponse> handleValidationException(
-      MethodArgumentNotValidException ex
+  public ResponseEntity<MethodArgumentNotValidResponse> handleValidationException(
+      MethodArgumentNotValidException e
   ) {
-    List<MeetingErrorResponse.FieldError> errors = ex.getBindingResult()
+    List<MethodArgumentNotValidResponse.FieldError> errors = e.getBindingResult()
         .getFieldErrors()
         .stream()
-        .map(error -> new MeetingErrorResponse.FieldError(
+        .map(error -> new MethodArgumentNotValidResponse.FieldError(
             error.getField(),
             error.getDefaultMessage(),
             error.getRejectedValue()))
         .collect(Collectors.toList());
 
     log.info("================== 필드 에러 ==================");
-    MeetingErrorResponse errorResponse = new MeetingErrorResponse(
+    MethodArgumentNotValidResponse errorResponse = new MethodArgumentNotValidResponse(
         HttpStatus.BAD_REQUEST.value(),
         "올바르지 않은 값이 있습니다.",
         errors
@@ -35,5 +51,11 @@ public class MeetingExceptionHandler {
     return ResponseEntity
         .badRequest()
         .body(errorResponse);
+  }
+
+  private ResponseEntity<MeetingErrorResponse> createErrorResponse(MeetingErrorCode errorCode) {
+    return ResponseEntity
+        .status(errorCode.getStatus())
+        .body(new MeetingErrorResponse(errorCode.getMessage()));
   }
 }
