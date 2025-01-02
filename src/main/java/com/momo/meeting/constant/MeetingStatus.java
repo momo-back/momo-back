44090e593d5a6f0c0c67a29a330273baab78d7d5
seokjin <wlsdldos00@gmail.com>
2025-01-02 18:50:55 +0900

    feat: 해당 모임 상태가 신청 가능한 상태인지 확인하는 메서드 추가

--- src/main/java/com/momo/meeting/constant/MeetingStatus.java
+++ src/main/java/com/momo/meeting/constant/MeetingStatus.java
@@ -1,5 +1,6 @@
 package com.momo.meeting.constant;
 
+import java.util.EnumSet;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,6 +10,15 @@ public enum MeetingStatus {
 
   RECRUITING("모집 중"),
   CLOSED("모집 완료");
+
+  // 상태가 추가될 것을 대비하여 모집 가능 상태와 모집 불가 상태를 구분
+  private static final EnumSet<MeetingStatus> ENABLE_RECRUITING = EnumSet.of(RECRUITING);
+  private static final EnumSet<MeetingStatus> UNABLE_RECRUITING = EnumSet.of(CLOSED);
+
+  // 모집 가능 상태인지 확인하는 메서드
+  public boolean isParticipate() {
+    return ENABLE_RECRUITING.contains(this);
+  }
 
   private final String description;
 }
