c203f71aebaff9175082ec52af4780eae2dddaaa
seokjin <wlsdldos00@gmail.com>
2025-01-02 18:53:54 +0900

    feat: 모임 참여 관련 Repository 구현

---
+++ src/main/java/com/momo/participation/repository/ParticipationRepository.java
@@ -0,0 +1,11 @@
+package com.momo.participation.repository;
+
+import com.momo.participation.entity.Participation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+  boolean findByUser_IdAndMeeting_Id(Long userId, Long id);
+}
