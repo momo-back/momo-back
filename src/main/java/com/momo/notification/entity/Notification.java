70a3639f4d335dc14f0780bd07fde50e2dee84ab
seokjin <wlsdldos00@gmail.com>
2025-01-04 03:34:17 +0900

    feat: user 테이블과 매핑된 필드를 FetchType을 Lazy로 설정

--- src/main/java/com/momo/notification/entity/Notification.java
+++ src/main/java/com/momo/notification/entity/Notification.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -28,9 +29,9 @@ public class Notification extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", nullable = false)
-  private User receiver;
+  private User user;
 
   @Column(nullable = false)
   private String content;
