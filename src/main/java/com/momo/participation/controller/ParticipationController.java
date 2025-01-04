a5a8f34a5882b5c72f81e164763cde6912684efa
seokjin <wlsdldos00@gmail.com>
2025-01-04 17:12:12 +0900

    refactor: 모임 생성 엔드포인트 변경

--- src/main/java/com/momo/participation/controller/ParticipationController.java
+++ src/main/java/com/momo/participation/controller/ParticipationController.java
@@ -17,7 +17,7 @@ public class ParticipationController {
 
   private final ParticipationService participationService;
 
-  @PostMapping("/{meetingId}/participate")
+  @PostMapping("/{meetingId}/participations")
   public ResponseEntity<Long> createParticipation(
       @AuthenticationPrincipal CustomUserDetails customUserDetails,
       @PathVariable Long meetingId
