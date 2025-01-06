bf79735d1d92dce98d14e56307bf9e19bfa2e25c
hanjarang <rkdsksehowl@naver.com>
2025-01-06 17:05:56 +0900

    [feature] 회원정보 수정 기능
    - 회원정보를 수정할 수 있는 기능
    - 수정 가능 항목 : 닉네임, 핸드폰 번호, 프로필 사진, 자기소개, MBTI
    
    Relates to: #51

---
+++ src/main/java/com/momo/user/dto/UserUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.momo.user.dto;
+
+import com.momo.profile.constant.Mbti;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequest {
+  private String nickname;
+  private String phone;
+  private String profileImageUrl;
+  private String introduction;
+  private Mbti mbti;
+}
