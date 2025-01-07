d1566d26650ecfe5c07a008b1e8e853d2731dc79
seokjin <wlsdldos00@gmail.com>
2025-01-07 22:29:18 +0900

    feat:참여 신청 id, meetingId, userId 정보를 포함한 참여한 모집글 목록을 조회하고 참여 신청한 순서대로 정렬하여 반환하는 쿼리 메서드 정의

--- src/main/java/com/momo/participation/repository/ParticipationRepository.java
+++ src/main/java/com/momo/participation/repository/ParticipationRepository.java
@@ -1,11 +1,50 @@
 package com.momo.participation.repository;
 
 import com.momo.participation.entity.Participation;
+import com.momo.participation.projection.AppliedMeetingsProjection;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
 
   boolean existsByUser_IdAndMeeting_Id(Long userId, Long id);
+
+  @Query(value =
+      "SELECT "
+          + "p.id as id, "
+          + "m.id as meetingId, "
+          + "m.user_id as userId, "
+          + "p.participation_status as participationStatus, "
+          + "m.title as title, "
+          + "m.location_id as locationId, "
+          + "m.latitude as latitude, "
+          + "m.longitude as longitude, "
+          + "m.address as address, "
+          + "m.meeting_date_time as meetingDateTime, "
+          + "m.max_count as maxCount, "
+          + "m.approved_count as approvedCount, "
+          + "categories.categories as category, "
+          + "m.content as content, "
+          + "m.thumbnail_url as thumbnailUrl "
+          + "FROM participation p "
+          + "JOIN meeting m ON p.meeting_id = m.id "
+          + "JOIN ("
+          + "    SELECT meeting_id, GROUP_CONCAT(category) as categories "
+          + "    FROM meeting_category "
+          + "    GROUP BY meeting_id"
+          + ") categories ON m.id = categories.meeting_id "
+          + "WHERE p.user_id = :userId "
+          + "AND p.id > :lastId "
+          + "ORDER BY p.id ASC "
+          + "LIMIT :pageSize",
+      nativeQuery = true)
+  List<AppliedMeetingsProjection> findAppliedMeetingsWithLastId(
+      @Param("userId") Long userId,
+      @Param("lastId") Long lastId,
+      @Param("pageSize") int pageSize
+  );
 }
