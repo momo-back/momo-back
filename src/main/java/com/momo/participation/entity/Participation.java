412271446b6127a5dabd0c1879a81bbee0ed2728
seokjin <wlsdldos00@gmail.com>
2025-01-07 22:27:47 +0900

    feat: 모임 참여 생성 메서드 구현 및 participationStatus 필드에 @Enumerated 어노테이션 추가

--- src/main/java/com/momo/participation/entity/Participation.java
+++ src/main/java/com/momo/participation/entity/Participation.java
@@ -6,6 +6,8 @@ import com.momo.participation.constant.ParticipationStatus;
 import com.momo.user.entity.User;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -36,6 +38,15 @@ public class Participation extends BaseEntity {
   @JoinColumn(name = "meeting_id", nullable = false)
   private Meeting meeting;
 
+  @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private ParticipationStatus participationStatus;
+
+  public static Participation createMeetingParticipant(User user, Meeting meeting) {
+    return Participation.builder()
+        .user(user)
+        .meeting(meeting)
+        .participationStatus(ParticipationStatus.PENDING)
+        .build();
+  }
 }
