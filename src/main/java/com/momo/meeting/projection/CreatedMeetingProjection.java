60fed4af4da9018c6af1cfda5c56a661763a57a8
seokjin <wlsdldos00@gmail.com>
2025-01-09 00:35:35 +0900

    feat: 개최한 모임 목록 쿼리의 반환값을 담을 프로젝션 정의

---
+++ src/main/java/com/momo/meeting/projection/CreatedMeetingProjection.java
@@ -0,0 +1,35 @@
+package com.momo.meeting.projection;
+
+import com.momo.meeting.constant.MeetingStatus;
+import java.time.LocalDateTime;
+
+public interface CreatedMeetingProjection {
+
+  Long getId();
+
+  Long getMeetingId();
+
+  MeetingStatus getMeetingStatus();
+
+  String getTitle();
+
+  Long getLocationId();
+
+  Double getLatitude();
+
+  Double getLongitude();
+
+  String getAddress();
+
+  LocalDateTime getMeetingDateTime();
+
+  Integer getMaxCount();
+
+  Integer getApprovedCount();
+
+  String getCategory();
+
+  String getContent();
+
+  String getThumbnailUrl();
+}
