3adeae045454c7604c2a9ec9f8fb9f1b040a6f69
seokjin <wlsdldos00@gmail.com>
2025-01-08 16:52:20 +0900

    feat: 모임 참여 신청 관련 에러 코드 정의

--- src/main/java/com/momo/participation/exception/ParticipationErrorCode.java
+++ src/main/java/com/momo/participation/exception/ParticipationErrorCode.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ParticipationErrorCode {
 
+  PARTICIPATION_NOT_FOUND("존재하지 않는 참여 신청입니다.", HttpStatus.NOT_FOUND),
+
+  NOT_PARTICIPATION_OWNER("본인의 참여 신청이 아닙니다.", HttpStatus.FORBIDDEN),
+
   INVALID_MEETING_STATUS("신청할 수 없는 모임 상태입니다.", HttpStatus.BAD_REQUEST),
 
   PARTICIPATE_SELF_MEETING_NOT_ALLOW(
