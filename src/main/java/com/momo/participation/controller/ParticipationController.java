de5804c38ecbb9f4f24b267692bca7662a4280b6
seokjin <wlsdldos00@gmail.com>
2025-01-08 16:51:33 +0900

    feat: 모임 상태 변경 엔드포인트 컨트롤러 구현

--- src/main/java/com/momo/participation/controller/ParticipationController.java
+++ src/main/java/com/momo/participation/controller/ParticipationController.java
@@ -1,15 +1,20 @@
 package com.momo.participation.controller;
 
+import com.momo.meeting.constant.MeetingStatus;
+import com.momo.participation.constant.ParticipationStatus;
 import com.momo.participation.dto.AppliedMeetingsResponse;
 import com.momo.participation.service.ParticipationService;
 import com.momo.user.dto.CustomUserDetails;
+import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,4 +48,17 @@ public class ParticipationController {
     );
     return ResponseEntity.ok(response);
   }
+
+  @PatchMapping
+  public ResponseEntity<Void> updateParticipationStatus(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails,
+      @PathVariable Long participationId,
+      @RequestBody @NotNull ParticipationStatus participationStatus
+  ) {
+    participationService.updateParticipationStatus(
+        customUserDetails.getId(), participationId, participationStatus
+    );
+    return ResponseEntity.ok().build();
+  }
+
 }
