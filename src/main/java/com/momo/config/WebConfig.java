c4e3be008d721812cd9b896779a4fdd97ff76d31
totorotail <jminkyun@gmail.com>
2025-01-17 03:35:06 +0900

    release: CORS 설정 추가 및 application.yml 파일 수정

--- src/main/java/com/momo/config/WebConfig.java
+++ src/main/java/com/momo/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.momo.config;
 import com.momo.config.interceptor.ProfileValidationInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addInterceptor(profileValidationInterceptor)
         .addPathPatterns("/api/v1/meetings")
         .addPathPatterns("/api/v1/notifications/**");
+  }
+
+  // CORS 설정 추가
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:5173")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
   }
 }
