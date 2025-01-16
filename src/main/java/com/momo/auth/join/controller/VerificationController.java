e260f97605d13e19d310101536198b3f69e0ba94
한자랑 <167011258+Hanjarang@users.noreply.github.com>
2025-01-17 02:43:02 +0900

    Revert "[fix] RefreshToken 관련 수정"

--- src/main/java/com/momo/auth/join/controller/VerificationController.java
+++ src/main/java/com/momo/auth/join/controller/VerificationController.java
@@ -1,27 +1,25 @@
 package com.momo.auth.join.controller;
 
-import com.momo.auth.join.service.JoinService;
 import com.momo.auth.join.service.VerificationService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-@RequiredArgsConstructor
 public class VerificationController {
 
   private final VerificationService verificationService;
-  private final JoinService joinService;
 
+  public VerificationController(VerificationService verificationService) {
+    this.verificationService = verificationService;
+  }
 
-  @PostMapping("/api/v1/users/signup/verify")
-  public ResponseEntity<String> verifyCode(@RequestParam String code) {
-    joinService.verifyCode(code);
-    return ResponseEntity.ok("이메일 인증이 완료되었습니다.");
+  @GetMapping("/api/v1/users/signup/verify")
+  public ResponseEntity<String> verifyUser(@RequestParam("token") String token) {
+    verificationService.verifyUser(token); // 서비스 호출
+    return ResponseEntity.ok("이메일 인증이 완료되었습니다!");
   }
 }
