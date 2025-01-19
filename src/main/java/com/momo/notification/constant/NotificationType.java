d81c355edc9ec989c9d4584d0f952372def229a6
seokjin <wlsdldos00@gmail.com>
2025-01-19 18:10:59 +0900

    style: 코드 너무 길어져 개행 추가

--- src/main/java/com/momo/notification/constant/NotificationType.java
+++ src/main/java/com/momo/notification/constant/NotificationType.java
@@ -8,12 +8,13 @@ import lombok.RequiredArgsConstructor;
 public enum NotificationType {
 
   // 모임 관련 (주최자)
-  NEW_PARTICIPATION_REQUEST("님이 모임 참여를 신청했습니다.", NotificationCategory.MEETING),
+  NEW_PARTICIPATION_REQUEST(
+      "님이 모임 참여를 신청했습니다.", NotificationCategory.MEETING),
 
   PARTICIPANT_LEFT("참가자 탈퇴", NotificationCategory.MEETING),
 
-  MEETING_EXPIRED(
-      "모임이 모임 시간이 만료되어 자동으로 삭제되었습니다.", NotificationCategory.MEETING),
+  MEETING_EXPIRED("모임이 모임 시간이 만료되어 자동으로 삭제되었습니다.",
+      NotificationCategory.MEETING),
 
   // 모임 관련 (참가자)
   PARTICIPANT_APPROVED("모임에 참여가 승인되었습니다.", NotificationCategory.MEETING),
